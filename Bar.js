ada7faa195ffe9cdf55a899a653f3603abf5d634
Nantanat Wattanakul <nantanat.w@ku.th>
2023-02-10 14:25:24 +0700

    initial version

---
+++ Bar.js
@@ -0,0 +1,11 @@
+import React, {useEffect,useState} from 'react'
+import { Link } from 'react-router-dom'
+import './Bar.css'
+
+function Bar() {
+  return (
+    <h1 className="car-chace">CarChace</h1>
+  )
+}
+
+export default Bar
